9e018583de1f9b6a43320659a83d039b014589f5
Priyam-Pramanik <86661996+Priyam-Pramanik@users.noreply.github.com>
2023-05-23 13:13:31 +0200

    Add files via upload
    final

--- main.js
+++ main.js
@@ -1,29 +1,34 @@
-function preload(){
+noseX = 0;
+noseY = 0;
 
+function preload(){
+    moustache = loadImage('https://i.postimg.cc/htPwgd2b/m.png');
 }
 function setup(){
     canvas = createCanvas(300,300);
     canvas.center();
     video = createCapture(VIDEO);
     video.size(300, 300);
-    video.hide;
+    video.hide();
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
 }
+function modelLoaded(){
+    console.log("posenet model initialized");
+}
 function gotPoses(results){
-    if(results>0){
+    if(results.length >0){
         console.log(results); 
         noseX = results[0].pose.nose.x-40;
          noseY = results[0].pose.nose.y;
     }
    
     }
-    function modelLoaded(){
-        console.log("posenet model initialized");
-    }
+   
 
 function draw(){
 image(video, 0, 0, 300, 300);
+image(moustache, noseX, noseY, 80, 35);
 }
 function takeSnapshot(){
     save('my_selfie.png');
